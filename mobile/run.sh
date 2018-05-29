fd751660b0dc718c5d712112257a7d9e04990099
TangXuanzhao <xuanzhaotang@gail.com>
2018-05-29 15:43:01 +0800

    add script

--- mobile/run.sh
+++ mobile/run.sh
@@ -1,8 +1,16 @@
-if [ ! -d "./docker-compose.yml" ];then
-download="curl -s https://raw.githubusercontent.com/sysu-SAAD-project/script/master/dev/docker-compose.yml -o docker-compose.yml"
-$download
-else
-echo "file exits"
-fi
-run="docker-compose up -d"
-$run
+# if [ ! -d "./docker-compose.yml" ];then
+# download="curl -s https://raw.githubusercontent.com/sysu-SAAD-project/script/master/dev/docker-compose.yml -o docker-compose.yml"
+# $download
+# else
+# echo "file exits"
+# fi
+# run="docker-compose up -d"
+# $run
+cd db/
+docker-compose up -d
+cd ../pc
+docker-compose up -d
+cd ../mobile
+docker-compose up -d
+cd ..
+docker-compose up -d

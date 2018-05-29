8695f550066aee0a643dd5efc57d96f230b8d19d
TangXuanzhao <xuanzhaotang@gmail.com>
2018-05-29 21:40:06 +0800

    change path

--- pc/run.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d "./docker-compose.yml" ];then
-download="curl -s https://raw.githubusercontent.com/sysu-SAAD-project/script/master/production/docker-compose.yml -o docker-compose.yml"
-$download
-else
-echo "file exits"
-fi
-run="docker-compose up -d"
-$run
--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
-if [ ! -d "./docker-compose.yml" ];then
-download="curl -s https://raw.githubusercontent.com/sysu-SAAD-project/script/master/docker-compose.yml -o docker-compose.yml"
-$download
-else
-echo "file exits"
-fi
-run="docker-compose up -d"
-$run
+# if [ ! -d "./docker-compose.yml" ];then
+# download="curl -s https://raw.githubusercontent.com/sysu-SAAD-project/script/master/dev/docker-compose.yml -o docker-compose.yml"
+# $download
+# else
+# echo "file exits"
+# fi
+# run="docker-compose up -d"
+# $run
+docker rm $(docker ps -a -q) -f
+cd db/
+docker-compose up -d
+cd ../pc
+docker-compose up -d
+cd ../mobile
+docker-compose up -d
+cd ..
+docker-compose up -d

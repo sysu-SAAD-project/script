8b3bd2725c54fc1f5d2f9a3dc8f3e146d7c0eae4
TangXuanzhao <xuanzhaotang@gmail.com>
2018-06-01 22:44:43 +0800

    add pull command

--- run.sh
+++ run.sh
@@ -6,6 +6,14 @@
 # fi
 # run="docker-compose up -d"
 # $run
+docker pull txzdream/sysu-activityplus-pc:dev
+docker pull xiaxzh/script:dev
+docker pull txzdream/sysu-activityplus-pc-mail-service:dev
+docker pull txzdream/sysu-activityplus-pc-service_end:dev
+docker pull phpmyadmin/phpmyadmin
+docker pull nginx
+docker pull rabbitmq:3-management
+echo y | docker image prune
 docker rm $(docker ps -a -q) -f
 cd db/
 docker-compose up -d
